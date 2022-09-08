27988ba6e631e77504325d39c62682f87efceb90
D. Mbedobe Kunji <danielbushbaby@gmail.com>
2022-09-08 14:12:11 +0000

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,8 +5,8 @@
  *
  * Return: Always 1 (Success)
  */
-int main(void) 
+int main(void)
 {
-  write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-  return (1); 
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
