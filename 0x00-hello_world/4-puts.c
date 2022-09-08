e48d8616c99af88e5f1e3fc2e7dca9f5d7940d49
D. Mbedobe Kunji <danielbushbaby@gmail.com>
2022-09-08 13:40:37 +0000

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,21 +1,12 @@
 #include <stdio.h>
-
-
-
 /**
-
  * main - Entry point
-
  *
-
  * Return: Always 0 (Success)
-
+ * Description - Printing lines
  */
-
 int main(void)
-  
 {
-  puts("\"Programming is like building a multilingual puzzle"); 
-  return (0);
-  
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
