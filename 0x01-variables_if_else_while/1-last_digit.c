70d10e8c583632c330fd1675c06bc4fe7dad22b1
MBEDOBE <danielbushbaby@gmail.com>
2022-09-09 09:48:41 -0700

    last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,7 +3,11 @@
 #include <stdio.h>
 /* more headers goes there */
 
-/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ * void - empty
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	int n;
