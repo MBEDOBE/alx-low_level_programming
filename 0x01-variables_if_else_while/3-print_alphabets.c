2d6a4467edf81dd6e37353758033b50c083fb7cd
MBEDOBE <danielbushbaby@gmail.com>
2022-09-09 11:38:36 -0700

    alphabets

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -5,17 +5,32 @@
  *
  * Return: Always 0 (Success)
  */
-int main(void) 
+int main(void)
+  
 {
- char a, b;  
- for (a = 'a'; a <= 'z'; a++)
+  
+  char a, b;
+  
+
+  
+  for (a = 'a'; a <= 'z'; a++)
+    
     {
+      
       putchar(a);
+      
     }
- for (b = 'A'; b <= 'Z'; b++)
+  
+  for (b = 'A'; b <= 'Z'; b++)
+    
     {
+      
       putchar(b);
+      
     }
-  putchar('\n'); 
-  return (0);  
+  
+  putchar('\n');
+  
+  return (0);
+  
 }
