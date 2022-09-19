e0a616941576d34872321114dd5d409f17026026
D. Mbedobe Kunji <danielbushbaby@gmail.com>
2022-09-19 20:14:12 +0000

    task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - function to change value via pointer
+ * @n : integer value parameter
+ * Return: Always 0.
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
