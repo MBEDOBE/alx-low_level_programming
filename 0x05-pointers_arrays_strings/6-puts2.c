92c55e021bd1e2927f60a35e1394a198e33ab509
D. Mbedobe Kunji <danielbushbaby@gmail.com>
2022-09-19 20:24:07 +0000

    task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * puts2 - skipping digits in array
+ * @str : array defined
+ * Return: Always 0.
+ */
+void puts2(char *str)
+{
+	int a;
+
+	for (a = 0; *(str + a) != '\0'; a++)
+	{
+		if (a % 2 == 0)
+			_putchar(*(str + a));
+	}
+	_putchar('\n');
+}
