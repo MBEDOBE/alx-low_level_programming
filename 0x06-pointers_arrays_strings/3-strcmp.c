29549b57f4da9a009d9f5c9ffa9b9587171235dd
D. Mbedobe Kunji <danielbushbaby@gmail.com>
2022-09-22 19:44:22 +0000

    task 3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strcmp - comparing strings
+ * @s1 : var pointer
+ * @s2 : pointer
+ * Return: Always 0.
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int a;
+
+	for (a = 0; s1[a] != '\0' && s2[a] != '\0'; a++)
+	{
+		if (s1[a] != s2[a])
+			return (s1[a] - s2[a]);
+	}
+	return (0);
+}
