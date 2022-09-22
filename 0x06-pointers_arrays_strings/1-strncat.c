55297d48fdb9f72ca29cea3a7c76b2fda9048b06
D. Mbedobe Kunji <danielbushbaby@gmail.com>
2022-09-22 19:41:43 +0000

    task 1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strncat - function to concatenate strings
+ * @dest : pointer
+ * @src : pointer
+ * @n : variable integer
+ * Return: Always 0.
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int a, b;
+
+	for (a = 0; dest[a] != '\0'; a++)
+		;
+	for (b = 0; b < n && src[b] != '\0'; b++)
+	{
+		dest[a + b] = src[b];
+	}
+	return (dest);
+}
