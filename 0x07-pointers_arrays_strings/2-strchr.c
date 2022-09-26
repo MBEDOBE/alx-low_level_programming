bd1cbbe47a2c99ff636461f4de793de835dba410
D. Mbedobe Kunji <danielbushbaby@gmail.com>
2022-09-26 21:36:45 +0000

    task 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#define NULL 0
+
+/**
+ * _strchr - function to locate a character
+ * @s: array strings
+ * @c: character that needs to be found
+ * Return: Always 0.
+ */
+char *_strchr(char *s, char c)
+{
+	int a;
+
+	for (a = 0; s[a] != '\0'; a++)
+	{
+		if (s[a] == c)
+			return (&s[a]);
+	}
+	if (c == '\0')
+		return (&s[a]);
+	return (NULL);
+}
