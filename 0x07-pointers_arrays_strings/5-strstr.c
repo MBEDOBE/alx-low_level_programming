3b069adf0c57a788b69e3f7872f03e37388735c8
D. Mbedobe Kunji <danielbushbaby@gmail.com>
2022-09-26 21:39:14 +0000

    task 5

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#define NULL 0
+
+/**
+ * _strstr - function finds the first occurrence of the substring
+ * @haystack: string to be searched & printed
+ * @needle: comparison
+ * Return: needle haystack comp.
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int a;
+	char *p;
+
+	if (*needle == '\0')
+		return (haystack);
+	for (; *haystack != '\0'; haystack++)
+	{
+		a = 0; /* restting a*/
+		p = haystack; /*restting p */
+		while (*p == needle[a])
+		{
+			a++;
+			p++;
+			if (needle[a] == '\0')
+				return (haystack);
+		}
+	}
+	return (NULL);
+}
