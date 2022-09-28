0fcb68e825c7707853a2b4e96c7ae447723ed014
D. Mbedobe Kunji <danielbushbaby@gmail.com>
2022-09-28 16:24:26 +0000

    task 6

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * prime_var - checking for prime n against each variable
+ * @n: integer value being passed for checking
+ * @var: variable being tested against for checking prime
+ * Return: Always 1 if prime 0 if not
+ */
+int prime_var(int n, int var)
+{
+	if (n % var == 0)
+		return (0);
+	if (var == n - 1)
+		return (1);
+	return (prime_var(n, var + 1));
+}
+
+/**
+ * is_prime_number - function to test the input integer as prime
+ * @n: integer value being passed to check for prime
+ * Return: Always 0.
+ */
+int is_prime_number(int n)
+{
+	if (n == 0)
+		return (1);
+	if (n == 1)
+		return (0);
+	if (n < 0)
+		return (0);
+	return (prime_var(n, 2));
+}
