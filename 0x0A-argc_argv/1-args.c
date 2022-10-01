1c0865d433ca98db5abe32a3badc1682db8b46c4
D. Mbedobe Kunji <danielbushbaby@gmail.com>
2022-10-01 18:58:59 +0000

    task 0

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - the entry point
+ * @argc: argument counter
+ * @argv: array of pointer to string
+ * Return: 0
+*/
+
+
+int main(int argc, char *argv[] __attribute__ ((unused)))
+
+{
+	printf("%d\n", argc - 1);
+
+	return (0);
+
+}
