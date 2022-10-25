c365b3d3d9be6c6fe91389fae3f8b8e843e8e053
D. Mbedobe Kunji <danielbushbaby@gmail.com>
2022-10-25 03:11:19 +0000

    Update lists.h

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -21,8 +21,8 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
-size_t print_listint(const listint_t *h);
-size_t listint_len(const listint_t *h);
+size_t print_listint(const listint_t *x);
+size_t listint_len(const listint_t *x);
 listint_t *add_nodeint(listint_t **head, const int n);
 listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
