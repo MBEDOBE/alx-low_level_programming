75123fcc0fb700c6a016825a24a84c89cbfc181a
D. Mbedobe Kunji <danielbushbaby@gmail.com>
2022-10-25 03:07:22 +0000

    Update lists.h

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -9,7 +9,7 @@
 
 /**
  * struct listint_s - singly linked list
- * @n: integer
+ * @y: integer
  * @next: points to the next node
  *
  * Description: singly linked list node structure
@@ -20,8 +20,8 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
-size_t print_listint(const listint_t *h);
-size_t listint_len(const listint_t *h);
+size_t print_listint(const listint_t *x);
+size_t listint_len(const listint_t *x);
 listint_t *add_nodeint(listint_t **head, const int y);
 listint_t *add_nodeint_end(listint_t **head, const int y);
 void free_listint(listint_t *head);
