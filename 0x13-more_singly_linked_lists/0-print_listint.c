509b2a5d815074c2c368dc88597d73d74b8b8a09
D. Mbedobe Kunji <danielbushbaby@gmail.com>
2022-10-25 03:14:25 +0000

    task 0

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -11,8 +11,8 @@ size_t print_listint(const listint_t *h)
 
 	while (h != NULL)
 	{
-		printf("%d\n", h->n); /* alternative way to write - (*h).n */
-		h = h->next; /* alternative wat to write (*h).next */
+		printf("%d\n", h->n); 
+		h = h->next; 
 		count++;
 	}
 	return (count);
