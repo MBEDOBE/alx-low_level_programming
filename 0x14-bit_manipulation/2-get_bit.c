bec5bea77b75c144f77dfca495862b434b326e6e
D. Mbedobe Kunji <danielbushbaby@gmail.com>
2022-10-28 00:37:11 +0000

    task 2

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * get_bit - function to get the bit at the given index
+ * @n: number to check the bit in
+ * @index: index to check the bit at
+ * Return: the bit at the index else -1
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	if ((sizeof(unsigned long int) * 8) < index)
+		return (-1);
+	n = n >> index;
+	return (n & 1);
+}
