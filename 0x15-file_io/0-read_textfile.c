e3f2678c56e23ac1b666bda2ed51441d659a1ce3
D. Mbedobe Kunji <danielbushbaby@gmail.com>
2022-10-31 22:53:49 +0000

    task 0

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,48 @@
+#include "main.h"
+
+/**
+ * read_textfile - function to read and print text file to stdout
+ * @filename: ptr to the file to read and print
+ * @letters: number of bytes to print
+ * Return: 0 if failed else number of bytes printed
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t reader, output, fd;
+	char *buff;
+
+	if (filename == NULL)
+		return (0);
+
+	buff = malloc(letters);
+	if (buff == NULL)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+
+	if (fd == -1)
+	{
+		free(buff);
+		return (0);
+	}
+
+	reader = read(fd, buff, letters);
+
+	if (reader == -1)
+	{
+		free(buff);
+		return (0);
+	}
+
+	output = write(STDOUT_FILENO, buff, reader);
+
+	if (output == -1 || output != reader)
+	{
+		free(buff);
+		return (0);
+	}
+
+	close(fd);
+	free(buff);
+	return (output);
+}
