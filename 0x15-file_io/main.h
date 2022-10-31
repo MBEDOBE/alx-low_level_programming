3af5c8e46ed9ac323fd55446b5a064f428fbd280
D. Mbedobe Kunji <danielbushbaby@gmail.com>
2022-10-31 22:52:03 +0000

    Create main.h

---
+++ 0x15-file_io/main.h
@@ -0,0 +1,19 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stdarg.h>
+#define BUFFSIZE 1024
+
+int _putchar(char c);
+ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
+void _err(int stat, ...);
+
+#endif
